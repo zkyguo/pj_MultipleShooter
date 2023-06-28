af23ce0da9329edc30619c194e4a76823233c412
Rui Guo <67830284+zkyguo@users.noreply.github.com>
2023-06-28 00:22:34 -0400

    Add MultiplayerSession plugin use to manage game sessions

--- Source/pj_MultipleShooter/pj_MultipleShooter.Build.cs
+++ Source/pj_MultipleShooter/pj_MultipleShooter.Build.cs
@@ -8,6 +8,6 @@ public class pj_MultipleShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay","OnlineSubSystemSteam", "OnlineSubSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay","OnlineSubSystemSteam", "OnlineSubSystem", "MultiplayerSessions" });
 	}
 }
